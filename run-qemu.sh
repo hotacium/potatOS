1104de99482cfdf84cfbabb290bba306ac2c6690
hmiya6 <h.miya.work@gmail.com>
2021-11-09 17:41:49 +0900

    run rust-gdb when running

--- run-qemu.sh
+++ run-qemu.sh
@@ -10,11 +10,15 @@ KERNEL_TARGET=$2
 qemu-img create -f raw $DISK 200M
 mkfs.fat -n 'POTATO OS' -s 2 -f 2 -R 32 -F 32 $DISK
 
-## mount_image 部分
+# mount_image 部分
 mkdir -p $MNT
 sudo mount -o loop $DISK $MNT
 
 sudo mkdir -p $MNT/EFI/BOOT
+
+# build bootloader
+cd potato_loader && cargo build 
+cd ../
 
 sudo cp $BOOTLOADER_TARGET $MNT/EFI/BOOT/BOOTX64.EFI
 sudo cp $KERNEL_TARGET $MNT/potatOS.elf
@@ -23,15 +27,5 @@ sleep 0.5
 sudo umount $MNT
 
 # run_image 部分
-
-# qemu-system-x86_64 \
-#     -m 1G \
-#     -drive if=pflash,format=raw,readonly,file=$DEVENV_DIR/OVMF_CODE.fd \
-#     -drive if=pflash,format=raw,file=$DEVENV_DIR/OVMF_VARS.fd \
-#     -drive if=ide,index=0,media=disk,format=raw,file=$DISK_IMG \
-#     -device nec-usb-xhci,id=xhci \
-#     -device usb-mouse -device usb-kbd \
-#     -monitor stdio \
-#     $QEMU_OPTS
-
-qemu-system-x86_64 -bios OVMF.fd -drive format=raw,file=$DISK # -s -S
+qemu-system-x86_64 -bios OVMF.fd -drive format=raw,file=$DISK -s -S &
+rust-gdb target/kernel_target/debug/potatOS.elf -ex "target remote :1234"
